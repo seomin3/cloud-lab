f407b750ce98eef871d157500b55678b9e048027
Jahyeon Koo <kjahyeon@gmail.com>
2017-07-18 14:01:09 +0900

    new: k8s with ansible environment

---
+++ kubeadm-ansible/userdata.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+systemctl stop apt-daily.service
+systemctl kill --kill-who=all apt-daily.service
+
+# wait until `apt-get updated` has been killed
+while ! (systemctl list-units --all apt-daily.service | fgrep -q dead)
+do
+  sleep 1;
+done
+
+# now proceed with own APT tasks
+apt-get install -yqq python
